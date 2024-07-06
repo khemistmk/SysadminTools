be83b81395efcecb059d924ad069ec82bb50620d
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-07-06 15:39:39 -0500

    fix edit

--- Start-UserSetup/Start-UserSetup.ps1
+++ Start-UserSetup/Start-UserSetup.ps1
@@ -120,7 +120,10 @@ function Start-UserSetup {
             function Remove-TaskbarApps { 
                 $apps = 'Microsoft Edge','Microsoft Store'
                 foreach ($appname in $apps){    
-                    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt()}
+                    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() |
+                    Where-Object {$_.Name -eq $appname}).Verbs() |
+                    Where-Object {$_.Name.replace('&','') -match 'Unpin from taskbar'} |
+                    ForEach-Object {$_.DoIt()}
                     Write-Host "App '$appname' unpinned from Taskbar"
                 }
             }
