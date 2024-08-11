f400563bd1a5765e2aceadd72bcc53220cf4beb2
Timothy Wilson <167370898+khemistmk@users.noreply.github.com>
2024-08-11 17:07:55 -0500

    update several items

--- Get-SystemStatus.ps1
+++ Get-SystemStatus.ps1
@@ -20,10 +20,11 @@ function Get-SystemStatus {
         $CIMComp = Get-CIMInstance -ClassName Win32_ComputerSystem -CimSession $cimsession
         $CIMVideo = Get-CimInstance -ClassName Win32_VideoController -CimSession $cimsession
         $RAMinfo = Get-CimInstance -Classname Win32_PhysicalMemory -CimSession $cimsession
+        $AVProd = Get-CimInstance -Namespace "root\SecurityCenter2" -ClassName AntivirusProduct -CimSession $cimsession
         
         #Reference called classes to assign variables
         $serialnumber = $CIMBIOS.SerialNumber
-        $computername = $CIMOS.PSComputerName
+        $computername = $CIMComp.Name
         $winver = $CIMOS.Caption
         $manufacturer = $CIMComp.manufacturer
         $RAM = $CIMComp | ForEach-Object {[math]::round($_.TotalPhysicalMemory /1GB)}
@@ -33,7 +34,6 @@ function Get-SystemStatus {
         $CPUInfo = (Get-CimInstance Win32_Processor -CimSession $cimsession).name
         $licensestatus = Get-CimInstance -ClassName SoftwareLicensingProduct -CimSession $cimsession -Filter "PartialProductKey IS NOT NULL" | Where-Object -Property Name -Like "Windows*"
         
-       
         #Registry calls and variable assignment
         $Winbuild = (Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('DisplayVersion')
         $OEM =  Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\OEMInformation" | Select-Object -Property Manufacturer,SupportHours,SupportPhone,SupportURL
@@ -47,7 +47,7 @@ function Get-SystemStatus {
         $fsvalue = (Get-ItemProperty -Path $fsPath -Name $fsName).HiberbootEnabled
 
         #Physical disk information
-        $Driveinfo = Get-PhysicalDisk
+        $diskinfo = Get-PhysicalDisk
     }
     process {
         #check license status
@@ -55,6 +55,9 @@ function Get-SystemStatus {
             $winactivation = "Activated"
         }
         #Round drive sizes based on capacity to standard sizes
+        if ($drivesize -lt "100") { $Drive = "$drivesize GB"}
+        if (($drivesize -gt "100")-and ($drivesize -lt "128")) { $Drive = "128 GB"}
+        if (($drivesize -gt "230")-and ($drivesize -lt "256")) { $Drive = "256 GB"}
         if (($drivesize -gt "459")-and ($drivesize -lt "468")) { $Drive = "500 GB"}
         if (($drivesize -gt "469") -and ($drivesize -lt "479")) { $Drive = "512 GB"}
         if (($drivesize -gt "929") -and ($drivesize -lt "1024")) { $Drive = "1 TB"}
@@ -73,27 +76,11 @@ function Get-SystemStatus {
         }
 
         #Get list of installed programs
-        $Programs = @()
-        $Programlist = @(
-                        "Adobe Acrobat",
-                        "Reader",
-                        "Foxit",
-                        "Microsoft Office",
-                        "Microsoft 365",
-                        "Project",
-                        "AutoDesk",
-                        "Navisworks",
-                        "VLC",
-                        "Chrome",
-                        "Firefox",
-                        "Sophos",
-                        "7-Zip",
-                         "Forticlient"
-        )
-        foreach ($p in $Programlist) {
-            $Programs += (Get-Package | Where-Object {$_.Name -like "*$p*"}).Name
-        }
-        $plist = $Programs | Out-String
+        $Programs = Get-Package
+        $plist = $Programs | 
+            Where-Object {$_.ProviderName -eq "Programs"} |
+                Select-Object -Property Name,Version |
+                    Sort-Object -Property Name
 
         #Set variable status based on parameter values
         if (!$Admin) {
@@ -148,20 +135,16 @@ function Get-SystemStatus {
         
         #Set Activation information PSCustomObject
         $Activationinfo = [PSCustomObject]@{
-            WindowsVersion      =   $winver
-            WindowsBuild        =   $Winbuild
-            WindowsActivation   =   $winactivation
+            Version             =   $winver
+            Build               =   $Winbuild
+            ActivationStatus    =   $winactivation
 
-        }
-
-        #Set Manufacturer informatino PSCustomObject
-        $Manufacturerinfo = [PSCustomObject]@{
-            Manufacturer        =   $manufacturer
-            Model               =   $model
         }
 
         #Set Hardware information PSCustomObject
         $Hardwareinfo = [PSCustomObject]@{
+            Manufacturer        =   $manufacturer
+            Model               =   $model
             CPU                 =   $CPUInfo
             TotalRAM            =   "$RAM GB"
             BIOSVersion         =   $biosver
@@ -177,15 +160,37 @@ function Get-SystemStatus {
 
         #Set Deployment Tasks information PSCustomObject
         $Deployment = [PSCustomObject]@{
+            Administrator   =   $Adminstatus
             Bitlocker       =   $bit
             Platformfolder  =   $platform
             OEMfolder       =   $OEMfolder
-            Administrator   =   $Adminstatus
             Dotnet3         =   $dotnet
             FastStartup    =   $faststart
             SmartDeploy     =   $SmartDeploy
         }
 
+        #Monitor timeout powercfg parameters
+        $monparam = @(
+            '/query'
+            'scheme_current' 
+             '7516b95f-f776-4464-8c53-06167f40cc99'
+            '3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e'
+        )
+        $montimeoutac,$montimeoutdc =   powercfg @monparam |
+                                            Select-Object -Last 2 -Skip 1 |
+                                                Foreach-Object {($_.Split(':')[1]) /60}
+        
+        #Sleep timeout powercfg parameters
+        $sleepparam = @(
+            '/query'
+            'scheme_current' 
+             '7516b95f-f776-4464-8c53-06167f40cc99'
+            '3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e'
+        )
+        $sleeptimeoutac,$sleeptimeoutdc =   powercfg @sleepparam |
+                                                Select-Object -Last 2 -Skip 1 |
+                                                    Foreach-Object {($_.Split(':')[1]) /60}
+        
         #Set Power Options informatino PSCustomObject
         $PowerOptions = [PSCustomObject]@{  
             MonitorTimeoutDC    =   "$montimeoutdc Minutes"
@@ -243,30 +248,43 @@ function Get-SystemStatus {
                                 DriverVersion	=	$DriverVersion    
                             }
                         }
+
+        #Set Antivirus infomation PSCustomObject          
+        $AVinfo =   foreach($Av in $AVProd){
+                        switch ($AV.productState) {
+                            "262144" {$defstatus = "Up to date" ;$rtstatus = "Disabled"}
+                            "262160" {$defstatus = "Out of date" ;$rtstatus = "Disabled"}
+                            "266240" {$defstatus = "Up to date" ;$rtstatus = "Enabled"}
+                            "266256" {$defstatus = "Out of date" ;$rtstatus = "Enabled"}
+                            "393216" {$defstatus = "Up to date" ;$rtstatus = "Disabled"}
+                            "393232" {$defstatus = "Out of date" ;$rtstatus = "Disabled"}
+                            "393488" {$defstatus = "Out of date" ;$rtstatus = "Disabled"}
+                            "397312" {$defstatus = "Up to date" ;$rtstatus = "Enabled"}
+                            "397328" {$defstatus = "Out of date" ;$rtstatus = "Enabled"}
+                            "397584" {$defstatus = "Out of date" ;$rtstatus = "Enabled"}
+                            default {$defstatus = "Unknown" ;$rtstatus = "Unknown"}
+                        }
+                        switch ($AV.DisplayName){
+                            'Sophos Intercept X' {$avversion = $programs | Where-Object {($_.Name -eq "Sophos Endpoint Agent") -and ($_.ProviderName -eq "Programs")} | Select-Object -ExpandProperty Version}
+                            'Sophos Home' {$avversion = $programs | Where-Object {($_.Name -eq "Sophos Home") -and ($_.ProviderName -eq "Programs")} | Select-Object -ExpandProperty Version}
+                            'Windows Defender' {$avversion = (Get-MpComputerStatus).AMProductVersion}
+                            default {$avversion = $programs | Where-Object { ($_.Name -like "*$($($av -split ' ')[0])*") -and ($_.ProviderName -eq "Programs")} | Select-Object -ExpandProperty Version}
+                        }
+                        [PSCustomObject]@{
+                            Name = $Av.DisplayName
+                            Status = $rtstatus
+                            Definition = $defstatus
+                            Version = $avversion
+                        }
+                    }
         
+        #Select enabled Antivirus Product
+        $AVenabled  =  $AVinfo | Where-Object {$_.Status -eq "Enabled"} | Sort-Object -Unique
         
-        #Monitor timeout powercfg parameters
-        $monparam = @(
-            '/query'
-            'scheme_current' 
-             '7516b95f-f776-4464-8c53-06167f40cc99'
-            '3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e'
-        )
-        $montimeoutac,$montimeoutdc =   powercfg @monparam |
-                                            Select-Object -Last 2 -Skip 1 |
-                                                Foreach-Object {($_.Split(':')[1]) /60}
-        
-        #Sleep timeout powercfg parameters
-        $sleepparam = @(
-            '/query'
-            'scheme_current' 
-             '7516b95f-f776-4464-8c53-06167f40cc99'
-            '3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e'
-        )
-        $sleeptimeoutac,$sleeptimeoutdc =   powercfg @sleepparam |
-                                                Select-Object -Last 2 -Skip 1 |
-                                                    Foreach-Object {($_.Split(':')[1]) /60}
-        
+        $Netinfo = Get-NetAdapter |
+            Where-Object {($_.Name -eq "Ethernet") -or ($_.Name -eq "Wi-Fi") -or ($_.Name -like "*Bluetooth*")} |
+                Select-Object -Property InterfaceDescription,MacAddress
+
         #Define keysfile location
         $keystext = Get-Content -Path $SaveLocation\keys.txt
         
@@ -276,33 +294,63 @@ function Get-SystemStatus {
         $Systeminfofile = "$SaveLocation\$computername.txt"
         
         #Append content to text file
-        Add-Content -Path $Systeminfofile "`n**************************************************************"
-        Add-Content -Path $Systeminfofile $($Compinfo | Format-List)
-        Add-Content -Path $Systeminfofile "Activation Information"
-        Add-Content -Path $Systeminfofile "`n**************************************************************"
-        Add-Content -Path $Systeminfofile $($Activationinfo | Format-List)
-        Add-Content -Path $Systeminfofile "Hardware Information"
-        Add-Content -Path $Systeminfofile "`n**************************************************************"
-        Add-Content -Path $Systeminfofile $($Manufacturerinfo | Format-List)
-        Add-Content -Path $Systeminfofile $($Hardwareinfo | Format-List)
-        Add-Content -Path $Systeminfofile "DIMM Information:"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
+        Add-Content -Path $Systeminfofile $($Compinfo | Format-List | Out-String)
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "ACTIVATION INFORMATION"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
+        Add-Content -Path $Systeminfofile $($Activationinfo | Format-List | Out-String)
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "HARDWARE INFORMATION"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
+        Add-Content -Path $Systeminfofile $($Hardwareinfo | Format-List | Out-String)
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "ANTIVIRUS INFORMATION:"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
+        Add-Content -Path $Systeminfofile "$($AVenabled | Out-String)"
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "DIMM INFORMATION"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
         Add-Content -Path $Systeminfofile $($RAMInfo | Out-String)
-        Add-Content -Path $Systeminfofile "Disk Information:"
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "DISK INFORMATION"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
         Add-Content -Path $Systeminfofile $($DriveInfo | Out-String)
-        Add-Content -Path $Systeminfofile "Graphics Processors:"
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "GRAPHICS INFORMATION"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
         Add-Content -Path $Systeminfofile $($Graphicsinfo | Out-String)
-        Add-Content -Path $Systeminfofile "Deployment Tasks"
-        Add-Content -Path $Systeminfofile "`n**************************************************************"
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "NETWORK INFORMATION"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
+        Add-Content -Path $Systeminfofile $($Netinfo | Out-String)
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "DEPLOYMENT TASKS"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
         Add-Content -Path $Systeminfofile "OEM Informaton:"
-        Add-Content -Path $Systeminfofile $($OEMinfo | Format-List)
-        Add-Content -Path $Systeminfofile $($Deployment | Format-List)
+        Add-Content -Path $Systeminfofile $($OEMinfo | Format-List | Out-String)
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile $($Deployment | Format-List | Out-String)
+        Add-Content -Path $Systeminfofile "----------"
         Add-Content -Path $Systeminfofile "Power Settings:"
-        Add-Content -Path $Systeminfofile $($PowerOptions | Format-List)
-        Add-Content -Path $Systeminfofile "Installed Software"
-        Add-Content -Path $Systeminfofile "`n**************************************************************"
-        Add-Content -Path $Systeminfofile $plist
+        Add-Content -Path $Systeminfofile $($PowerOptions | Format-List | Out-String)
+        Add-Content -Path $Systeminfofile "----------"
+        Add-Content -Path $Systeminfofile "**************************************************************"
+        Add-Content -Path $Systeminfofile "INSTALLED SOFTWARE"
+        Add-Content -Path $Systeminfofile "--------------------------------------------------------------"
+        Add-Content -Path $Systeminfofile $($plist | Out-String)
+        (Get-Content $Systeminfofile) | Where-Object {$_.trim() -ne "" } | set-content $Systeminfofile
     }
     end {
+
     }
 }
 Get-SystemStatus
